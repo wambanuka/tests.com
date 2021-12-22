23f5817dd376d6c7af1534a604c230524d10bde4
wambanuka <chaykayulllll@gmail.com>
2021-12-23 01:39:17 +0300

    hk

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_list, name='test_list'),
-    path('test/', views.test, name='test'),
+    path('', views.test, name='test'),
+
 ]
